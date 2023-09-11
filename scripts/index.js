91e61764264e099d87df0c7aadc9f87617cc419a
Erfan Maleki <mlkyrfan85@gmail.com>
2023-09-12 00:01:00 +0330

    version 2.0 =>delete button

--- scripts/index.js
+++ scripts/index.js
@@ -4,13 +4,24 @@ function renderPage() {
   const container = document.querySelector(".chores-container");
   container.innerHTML = "";
   choresList.forEach((element) => {
-    container.innerHTML += `<p data-item='${element}'>&bull;${element}</p>`;
+    container.innerHTML += `
+    <div class="chores-bar" id="${element.No}">
+    <p>&bull;${element.name}</p>
+    <button class="remove-button" data-id="${element.No} ">
+    remove</button>
+    </div>`;
+    document.querySelectorAll(".remove-button").forEach((element) => {
+      element.addEventListener("click", () => {
+        removeChores(element.dataset.id);
+      });
+    });
   });
 }
+
 function addChores() {
   const inputBoxHtml = document.getElementById("chores-input");
   if (!(inputBoxHtml.value === "")) {
-    choresList.push(inputBoxHtml.value);
+    choresList.push({ name: inputBoxHtml.value, No: choresList.length + 1 });
     inputBoxHtml.value = "";
     renderPage();
   }
@@ -20,5 +31,14 @@ function resetChores() {
   choresList = [];
   renderPage();
 }
+
+function removeChores(id) {
+  const newList = choresList.filter((element) => {
+    return !(element.No === Number(id));
+  });
+  choresList = newList;
+  renderPage();
+}
+
 document.getElementById("add-button").addEventListener("click", addChores);
 document.getElementById("delete-button").addEventListener("click", resetChores);
